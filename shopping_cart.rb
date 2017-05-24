0c0573db3068abc6f032a84d36933685e2ef07ee
mikesmall <michael.l.a.small@gmail.com>
2017-05-24 01:28:59 -0400

    Shopping cart code ready for first test outputs

--- shopping_cart.rb
+++ shopping_cart.rb
@@ -1,28 +1,36 @@
 require_relative 'product_to_be_purchased.rb'
 
 # Each shopping cart has a collection of products.
-
 @@products_in_cart = []
 
-# It should also have the following functionality:
-
-#   - add a product to the cart
-
+# add a product to the cart
 def add_product(name, price)
   new_product = Product.new(name, price)
   @@products_in_cart << Product
   new_product
 end#add_product
 
-#   - remove a product from the cart
-
+# remove a product from the cart
 def remove_product(name)
   @@products_in_cart.delete(Product)
 end#remove_product
 
-#   - add up the total cost of all products in the cart before tax
+# add up the total cost of all products in the cart before tax
+def cumulative_total_before_tax
+  amount = 0.0
+  @@products_in_cart.each do |product|
+    amount += product.price
+  end#each
+  return amount
+end#cumulative_total_before_tax
 
+# add up the total cost of all products in the cart after tax
+def cumulative_total_after_tax
+  amount = 0.0
+  @@products_in_cart.each do |product|
+    amount += product.total_price
+  end#each
+  return amount
+end#cumulative_total_after_tax
 
-
-
-#   - add up the total cost of all products in the cart after tax
+# --- Test outputs ---
