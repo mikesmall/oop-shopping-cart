f26a75f63c165923406c0182e4fdb9dd8dfd5ade
mikesmall <michael.l.a.small@gmail.com>
2017-05-24 01:08:18 -0400

    'Product to be Purchased' file feels done

---
+++ product_to_be_purchased.rb
@@ -0,0 +1,28 @@
+# Each product has a name, base price, and tax rate.
+
+# There should also be a method to calculate and
+# return the product's total price based on the
+# base price and tax rate.
+
+class Product
+
+  attr_accessor :name, :price
+  attr_reader :tax
+
+  @@tax = 0.13 # that's 13%
+
+  def initialize (name, price)
+    @name = name
+    @price = price
+  end#initialize
+
+  def total
+    total_price = (@price * @@tax) + @price
+    return total_price
+  end#total
+
+end#Product
+
+# -- Test outputs --
+# tamagotchi = Product.new("Tamagotchi", 4.99)
+# puts tamagotchi.total
---
+++ shopping_cart.rb
@@ -0,0 +1,8 @@
+require_relative 'product_to_be_purchased.rb'
+
+# Each shopping cart has a collection of products.
+# It should also have the following functionality:
+#   - add an product to the cart
+#   - remove an product from the cart
+#   - add up the total cost of all products in the cart before tax
+#   - add up the total cost of all products in the cart after tax
